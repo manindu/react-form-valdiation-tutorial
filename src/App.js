3268850eaf1a22e15466773b8b19fdc9601f454b
Manindu W <manindu@linearsquared.com>
2019-01-20 13:13:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import Input from './Components/Input';
 
 class App extends Component {
+  onSubmit = (values) => {
+    alert(JSON.stringify(values));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: ''
+        }}
+        validationSchema={Yup.object().shape({
+          name: Yup.string().required(),
+          email: Yup.string().email().required(),
+          password: Yup.string().required()
+        })}
+        onSubmit={this.onSubmit}
+        render={({ handleChange, handleBlur, values, errors, handleSubmit, touched }) => (
+          <div style={{ padding: 100 }}>
+            <h2>Sign Up</h2>
+            <Input
+              id="name"
+              label="name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.name && errors.name}
+            />
+            <Input
+              id="email"
+              label="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.email && errors.email}
+            />
+            <Input
+              id="password"
+              label="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={touched.password && errors.password}
+              type="password"
+            />
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+        )}
+      />
     );
   }
 }
